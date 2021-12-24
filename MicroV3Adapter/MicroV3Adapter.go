b1d34522d3cb545aba20beb910d9d7106da8edfb
shanlongpan <shanlongpan@100tal.com>
2021-12-24 18:25:49 +0800

    save

--- MicroV3Adapter/MicroV3Adapter.go
+++ MicroV3Adapter/MicroV3Adapter.go
@@ -11,6 +11,8 @@ import (
 	_ "github.com/asim/go-micro/plugins/client/grpc/v3"
 	"github.com/asim/go-micro/plugins/registry/etcd/v3"
 	_ "github.com/asim/go-micro/plugins/server/grpc/v3"
+	"github.com/asim/go-micro/plugins/wrapper/breaker/hystrix/v3"
+	"github.com/asim/go-micro/plugins/wrapper/select/shard/v3"
 	"github.com/asim/go-micro/v3"
 	"github.com/asim/go-micro/v3/client"
 	"github.com/asim/go-micro/v3/registry"
@@ -18,6 +20,8 @@ import (
 )
 
 var clientInstance microv3.MicroV3Service
+
+const hashKey = "hash_key"
 
 func init() {
 	// etcd 服务注册和发现以后改成环境变量配置
@@ -27,6 +31,7 @@ func init() {
 		micro.Name("micro-v3-learn"),
 		micro.Version("latest"),
 		micro.Registry(reg),
+		micro.WrapClient(hystrix.NewClientWrapper(), shard.NewClientWrapper(hashKey)),
 		//micro.Client(grpc.NewClient()),
 	)
 
@@ -38,10 +43,9 @@ func init() {
 
 }
 
-// 负载均衡使用默认，以后再完善
-func Call(ctx context.Context, req *microv3.CallRequest,opts ...client.CallOption) (*microv3.CallResponse, error) {
-	// 日志和调用链追踪待补
-	rsp, err := clientInstance.Call(ctx, req)
+//
+func Call(ctx context.Context, req *microv3.CallRequest, opts ...client.CallOption) (*microv3.CallResponse, error) {
+	rsp, err := clientInstance.Call(ctx, req, opts...)
 	if err != nil {
 		// 打日志
 		fmt.Println("Error calling MicroV3: ", err)
@@ -51,7 +55,7 @@ func Call(ctx context.Context, req *microv3.CallRequest,opts ...client.CallOptio
 }
 
 func Stream(ctx context.Context, in *microv3.StreamingRequest, opts client.CallOption) (microv3.MicroV3Service_StreamService, error) {
-	// 日志和调用链追踪待补
+	//
 	//rsp, err := clientInstance.Stream(ctx, in,opts)
 	//if err != nil {
 	//	// 打日志
@@ -63,5 +67,5 @@ func Stream(ctx context.Context, in *microv3.StreamingRequest, opts client.CallO
 }
 
 func PingPong() {
-	// 日志和调用链追踪待补
+
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,9 @@ require (
 	github.com/asim/go-micro/plugins/client/grpc/v3 v3.0.0-20210630062103-c13bb07171bc
 	github.com/asim/go-micro/plugins/registry/etcd/v3 v3.7.0
 	github.com/asim/go-micro/plugins/server/grpc/v3 v3.7.0
+	github.com/asim/go-micro/plugins/wrapper/breaker/hystrix/v3 v3.7.0
+	github.com/asim/go-micro/plugins/wrapper/select/roundrobin/v3 v3.7.0 // indirect
+	github.com/asim/go-micro/plugins/wrapper/select/shard/v3 v3.7.0
 	github.com/asim/go-micro/v3 v3.7.0
 	google.golang.org/genproto v0.0.0-20210821163610-241b8fcbd6c8 // indirect
 	google.golang.org/grpc/examples v0.0.0-20211015201449-4757d0249e2d // indirect
